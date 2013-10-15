015feb5759fb9f764ce71f8208bbe4b134de6eb7
Nikhil Taneja <nikhil.a.taneja@gmail.com>
2013-10-14 19:15:22 -0600

    added color

--- web_guesser.rb
+++ web_guesser.rb
@@ -24,7 +24,17 @@ get '/' do
 
   guess = params["guess"].to_i
   message = check_guess(guess)
+  
+  if message == "Way too low!" || message == "Way too high!"
+    color = "red"
+  elsif message == "Too low!" || message == "Too high!"
+    color = "Salmon"
+  elsif message == "You got it right! The SECRET NUMBER is #{SECRET_NUMBER}"
+    color = "green"
+  else
+    color = "white"
+  end
 
-  erb :index, :locals => {:number => SECRET_NUMBER, :message => message}
+  erb :index, :locals => {:number => SECRET_NUMBER, :message => message, :color => color}
 
 end
